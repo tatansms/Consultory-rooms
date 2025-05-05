f4697bf2269e16fe352d06765ec11685a9ae3ec6
tatansms <sebas23012003@gmail.com>
2025-05-05 18:05:34 -0500

    Complemento de la capa de seguridad

--- src/main/java/com/datos/umag/consultorioMedico/security/SecurityConfig.java
+++ src/main/java/com/datos/umag/consultorioMedico/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -29,4 +31,9 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/com/datos/umag/consultorioMedico/security/service/UserInfoDetail.java
+++ src/main/java/com/datos/umag/consultorioMedico/security/service/UserInfoDetail.java
@@ -34,12 +34,12 @@ public class UserInfoDetail implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
